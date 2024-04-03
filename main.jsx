c23363485cca7b4115d56554862e8f89c415afa8
Lucas Roman <lucasroman@gmail.com>
2024-04-03 13:02:34 -0300

    Add App component to main.jsx
    Also add React StrictMode to render.

--- main.jsx
+++ main.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
+import App from './components/App';
 
 const root = createRoot(document.getElementById('root'));
-root.render('Hi from react!');
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
