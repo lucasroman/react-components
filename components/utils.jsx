50d0c12d3020a5be449a770f0f4abf1c41f03b31
Lucas Roman <lucasroman@gmail.com>
2024-04-19 23:12:45 -0300

    Update utils.jsx component
    Now work with data from Data.jsx

--- components/utils.jsx
+++ components/utils.jsx
@@ -1,6 +1,6 @@
 // Return image url for source
 export function getImageUrl(person, size = 's') {
   return (
-    "https://www.georgetown.edu/wp-content/uploads/2022/02/Jkramerheadshot-scaled-e1645036825432-" + person.resolution + "-c-default.jpg"
+    'https://i.imgur.com/' + person.imageId + 's.jpg'
   );
 }
