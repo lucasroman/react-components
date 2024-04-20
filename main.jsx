15a6316aa57bbe00e1acf41fd0c3d4b7c3cbc1c5
Lucas Roman <lucasroman@gmail.com>
2024-04-19 23:15:02 -0300

    Change main to load App component
    Replace PackingList by App component.

--- main.jsx
+++ main.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import PackingList from '/components/PackingList';
+import App from '/components/App.jsx';
 import '/styles/katsuko.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <PackingList />
+    <App />
   </React.StrictMode>
 );
